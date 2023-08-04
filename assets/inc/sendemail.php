d17f2e93dcbc656fc45a86f808ae990c26b43ca3
Rahul Fnu <frahul@schools.nyc.gov>
2023-08-04 15:43:52 -0400

    Initial Commit

--- assets/inc/sendemail.php
+++ assets/inc/sendemail.php
@@ -7,15 +7,9 @@ define( "RECIPIENT_EMAIL", "mrsaini281@gmail.com" );
 // Read the form values
 $success = false;
 $name = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['name'] ) : "";
-$fname = isset( $_POST['fname'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['fname'] ) : "";
-$lname = isset( $_POST['lname'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['lname'] ) : "";
 $senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
 $phone = isset( $_POST['phone'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
-$services = isset( $_POST['services'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['services'] ) : "";
-$date = isset( $_POST['date'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['date'] ) : "";
-$time = isset( $_POST['time'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['time'] ) : "";
 $subject = isset( $_POST['subject'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
-$website = isset( $_POST['website'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['website'] ) : "";
 $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
 
 
@@ -31,9 +25,6 @@ $body .= 'Email: '. $senderEmail . "\r\n";
 
 
 if ($phone) {$body .= 'Phone: '. $phone . "\r\n"; }
-if ($services) {$body .= 'Services: '. $services . "\r\n"; }
-if ($date) {$body .= 'Date: '. $date . "\r\n"; }
-if ($time) {$body .= 'Time: '. $time . "\r\n"; }
 if ($subject) {$body .= 'Subject: '. $subject . "\r\n"; }
 if ($website) {$body .= 'Website: '. $website . "\r\n"; }
 
