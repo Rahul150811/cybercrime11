b17c333fd22c5b61c1b91e72bc222c2e970891a7
Rahul150811 <fnu.rahul25@student.qcc.cuny.edu>
2023-08-05 20:33:26 -0400

    Initial Commit

--- assets/js/virustotal.js
+++ assets/js/virustotal.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-  $("#scan").click(function() {
-    var fileOrUrl = $("#file-or-url").val();
-    var apiKey = "1a82c50b0b02b73e8e038fb496f4208972ea672c440ada17cc56fdb5c0c838dc";
-    var url = "https://www.virustotal.com/api/v3/files/scan";
-    var data = {"apikey": apiKey, "file": fileOrUrl};
-    $.ajax({
-      url: url,
-      data: data,
-      type: "POST",
-      success: function(response) {
-        var results = JSON.parse(response);
-        $("#results").html(results);
-      },
-      error: function(error) {
-        console.log(error);
-      }
+    $("#check").click(function() {
+      var link = $("#link").val();
+      var apiKey = "1a82c50b0b02b73e8e038fb496f4208972ea672c440ada17cc56fdb5c0c838dc";
+      var url = "https://www.virustotal.com/api/v3/urls/scan";
+      var data = {"apikey": apiKey, "url": link};
+      $.ajax({
+        url: url,
+        data: data,
+        type: "POST",
+        success: function(response) {
+          var results = JSON.parse(response);
+          $("#results").html(results["positives"] > 0 ? "Malicious" : "Safe");
+        },
+        error: function(error) {
+          console.log(error);
+        }
+      });
     });
   });
-});
