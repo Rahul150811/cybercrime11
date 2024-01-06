20b05ca7cb43482ba26a140b78dc828fe0a78fe8
Rahul150811 <fnu.rahul25@student.qcc.cuny.edu>
2024-01-06 15:01:34 -0500

    Initial Commit

--- assets/js/automate_topbar.js
+++ assets/js/automate_topbar.js
@@ -1,7 +1,7 @@
 function changeUpdatesText() {
   var updatesSection = document.querySelector(".topbar-one__left");
   var updatesText = updatesSection.querySelector("p");
-  updatesText.innerText = "New Cyber Risk Assessment Tool is Available now";
+  updatesText.innerText = "Level up your online safety with our quick and easy ";
 }
 
 window.addEventListener("load", changeUpdatesText);
