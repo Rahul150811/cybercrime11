2305a0acfe0560af5928823f8a7985b1d46e38de
Rahul150811 <fnu.rahul25@student.qcc.cuny.edu>
2024-01-17 18:01:50 -0500

    Initial Commit

--- assets/js/buttonclickedtrackergoogleanalytics.js
+++ assets/js/buttonclickedtrackergoogleanalytics.js
@@ -1,9 +1,8 @@
-<script>
-    document.getElementById("report-button").addEventListener("click", function() {
-        // Send an event to Google Analytics
-        gtag('event', 'button_click', {
-            'event_category': 'Report Button',
-            'event_label': 'Report Now Button Clicked'
-        })
-    });
-</script>
+document.getElementById("report-button").addEventListener("click", function() {
+    // Send an event to Google Analytics
+    gtag('event', 'button_click', {
+        'event_category': 'Report Button',
+        'event_label': 'Report Now Button Clicked'
+    })
+});
+    
